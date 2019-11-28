a57fdc9ffc4f4f7b1ca90b1d1d5aa0c8c96d6566
Camilo Guio <camiloguio@gmail.com>
2019-11-28 18:32:13 -0500

    Radio buttonms cuotas finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,33 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'colsanitasLanding';
+
+  public btnCuotas = [
+
+    {seis:true},
+    {doce:false},
+    {dieciocho:false},
+    {veinticuatro:false},
+    {treseis:false},
+    {cuatrocho:false}
+
+  ]
+
+  changeButton(btn:any){
+
+    /* btn = true; */
+    console.log("btn", btn);
+    
+    this.btnCuotas.forEach(element => {
+      console.log(element);      
+      
+      
+    });
+    console.log("---- Other ----");
+    
+  }
+
 }
