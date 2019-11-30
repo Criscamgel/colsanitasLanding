3fadd30e8fb29d6b340bc0f20a436a8e8c6c685e
Camilo Guio <camiloguio@gmail.com>
2019-11-29 20:32:14 -0500

    Calculadora Finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,29 +9,102 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'colsanitasLanding';
 
-  public btnCuotas = [
+  cuotas;
+  valorSolicitado = 0;
+  minVlr = 800000;
+  maxVlr = 20000000;
+  vlrCuota = 0;
+  descuento = 0.21;
+  tasa = 0.24;
+  
+  vlrCuotaSs;
+  seguroCta;
+  seguroTotal;
 
-    {seis:true},
-    {doce:false},
-    {dieciocho:false},
-    {veinticuatro:false},
-    {treseis:false},
-    {cuatrocho:false}
 
-  ]
+  currencyInputChanged(value) {
+    var num = value.replace(/[$,]/g, "");  
+    return Number(num);
+  }  
 
-  changeButton(btn:any){
-
-    /* btn = true; */
-    console.log("btn", btn);
+  changeButton(val){
+    let cuota = Number(val.value);
+    let nmv = Math.pow((1 + this.tasa),(1/12))-1;
+    let seguro =  (1200 / 1000000) * this.valorSolicitado;    
     
-    this.btnCuotas.forEach(element => {
-      console.log(element);      
-      
-      
-    });
-    console.log("---- Other ----");
+    switch (cuota) {
+      case 6:
+        var vlrCuota;
+        var vlrDescuento = Math.round(this.valorSolicitado * this.descuento);
+        var seguroCta = Math.round(seguro * cuota);        
+
+        var vlrActual = Math.round(this.valorSolicitado - vlrDescuento);                
+        var vlrPartuno = vlrActual * nmv;
+        var vlrPartdos = Math.pow((1 + nmv), - cuota)
+        vlrPartdos = 1 - vlrPartdos;
+        vlrCuota = vlrPartuno / vlrPartdos;
+        vlrCuota = Math.round(vlrCuota);
+        
+        console.log("vlrCuota", vlrCuota);        
+               
+        var vlrPartunoSeg = seguroCta * nmv;      
+        var vlrPartdosSeg = Math.pow((1 + nmv), - cuota)
+        vlrPartdosSeg = 1 - vlrPartdosSeg;
+        seguroCta = (Math.round(vlrPartunoSeg) / vlrPartdosSeg);
+        seguroCta = Math.round(seguroCta);
+
+        console.log("seguroCta", seguroCta); 
+
+        this.vlrCuota = Math.round(vlrCuota + seguroCta);
+                
+        break;
+      case 12:
+        this.vlrCuota = Math.round(this.valorSolicitado/cuota) 
+        break;
+
+      case 18:
+        this.vlrCuota = Math.round(this.valorSolicitado/cuota) 
+        break;
+
+      case 24:
+        this.vlrCuota = Math.round(this.valorSolicitado/cuota) 
+        break;
+
+      case 36:
+        var vlrCuota;
+        var vlrDescuento = Math.round(this.valorSolicitado * this.descuento);
+        var seguroTotal = Math.round(seguro * cuota);
+        this.seguroTotal = seguroTotal;
+        console.log(this.seguroTotal);
+                
+
+        var vlrActual = Math.round(this.valorSolicitado - vlrDescuento);                
+        var vlrPartuno = vlrActual * nmv;
+        var vlrPartdos = Math.pow((1 + nmv), - cuota)
+        vlrPartdos = 1 - vlrPartdos;
+        vlrCuota = vlrPartuno / vlrPartdos;
+        vlrCuota = Math.round(vlrCuota);
+
+        this.vlrCuotaSs = vlrCuota;
+        
+        console.log("vlrCuota", vlrCuota);        
+               
+        var vlrPartunoSeg = seguroTotal * nmv;      
+        var vlrPartdosSeg = Math.pow((1 + nmv), - cuota)
+        vlrPartdosSeg = 1 - vlrPartdosSeg;
+        seguroCta = (Math.round(vlrPartunoSeg) / vlrPartdosSeg);
+        seguroCta = Math.round(seguroCta);
+        this.seguroCta = seguroCta;
+
+        console.log("seguroCta", seguroCta); 
+
+        this.vlrCuota = Math.round(vlrCuota + seguroCta);        
+
+        break;
     
+      default:
+        break;
+    }
   }
 
 }
