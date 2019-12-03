bcf0c5a27426c80404c5c89d56ad7edb2ab9b1e4
Camilo Guio <camiloguio@gmail.com>
2019-12-02 19:19:24 -0500

    Rutas Generadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,24 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+/* Routes */
+import { RouterModule } from '@angular/router';
+import { ROUTES } from './app.routes';
+import { AyudaComponent } from '../components/ayuda/ayuda.component';
+import { HomeComponent } from '../components/home/home.component';
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AyudaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    RouterModule.forRoot( ROUTES, { useHash:true } )
   ],
   providers: [],
   bootstrap: [AppComponent]
